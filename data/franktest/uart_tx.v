b6892e064e878d51a502ccfad92df889adad265e
Frank Dekervel <frank@kapernikov.com>
2019-04-09 22:27:39 +0200

    clock divider support

--- data/franktest/uart_tx.v
+++ data/franktest/uart_tx.v
@@ -10,10 +10,16 @@ module uart_tx(i_clock, i_data, i_act, o_signal, o_busy);
 
     reg [8:0] shift_register;
     reg [3:0] state_register;
+    reg [31:0] counter;
+
+    parameter BAUDRATE = 56600;
+    parameter HZ = 200_000_000;
+    parameter DIVIDER = HZ / BAUDRATE;
 
     initial o_busy = 0;
-    initial shift_register = 9'b0;
+    initial shift_register = 9'hFF;
     initial state_register = 4'b0;
+    initial counter = 0;
 
     assign o_signal = shift_register[0];
 
@@ -23,30 +29,40 @@ module uart_tx(i_clock, i_data, i_act, o_signal, o_busy);
             begin
                 shift_register[8:1] <= i_data;
                 shift_register[0] <= 0;
-                o_busy <= 1;                
-            end
-      else if (o_busy && state_register < 4'd8)
-            begin
-                state_register <= state_register + 1;
-                shift_register[7:0] <= shift_register[8:1];
-
-            end
-      else if (o_busy && state_register == 4'd8)
-            begin
-                shift_register[0] <= 1;
-                state_register <= state_register + 1; 
-                    // we are sending the stop bit. high value, but we are still busy because
-                    // we don't start sending the new byte yet.
-                    // is this right ?
+                o_busy <= 1;
+                counter <= 0;
             end
       else
-            begin
-              o_busy <= 0;
-              state_register <= 0;
-              shift_register[0] <= 1;
-            end
+        if (counter >= DIVIDER - 1)
+        begin
+            counter <= 0;
+            if (o_busy && state_register < 4'd8)
+                    begin
+                        state_register <= state_register + 1;
+                        shift_register[7:0] <= shift_register[8:1];
 
+                    end
+            else if (o_busy && state_register == 4'd8)
+                    begin
+                        shift_register[0] <= 1;
+                        state_register <= state_register + 1; 
+                            // we are sending the stop bit. high value, but we are still busy because
+                            // we don't start sending the new byte yet.
+                            // is this right ?
+                    end
+            else
+                    begin
+                    o_busy <= 0;
+                    state_register <= 0;
+                    shift_register[0] <= 1;
+                    end
+        end
+        else
+            begin
+                counter <= counter + 1;
+            end
     end
+
 
 endmodule;
 /* verilator lint_on DECLFILENAME */
